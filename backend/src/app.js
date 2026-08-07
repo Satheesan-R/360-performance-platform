d6279591532b3a3bba34c402b1821fafd7964b1f
satheesan999 <rajasekarsatheesan2003@gmail.com>
2026-08-07 20:43:59 +0530

    Update the login page css

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,12 +6,11 @@ const employeeRoutes = require('./modules/employees/employee.routes');
 
 const app = express();
 
-app.use(cors({ origin: env.frontendUrl, credentials: true }));
-app.use(express.json({ limit: '20kb' }));
-
-app.get('/api/health', (req, res) => {
-  res.status(200).json({ success: true, message: 'API is healthy' });
-});
+app.use(cors(
+  {  origin: env.frontendUrl, credentials: true }
+  ));
+app.use(express.json({ limit: '20kb'}
+   ));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/employees', employeeRoutes);
